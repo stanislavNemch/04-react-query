9360fb297e16e5f00304b72528171bc2d543b428
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-22 11:38:49 +0300

    add some comments

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,5 +1,4 @@
 // Інтерфейс окремого фільму, що приходить у масиві `results`.
-// ТУТ зберігаємо тільки "дані домену" (Movie), а не форми відповіді API.
 export interface Movie {
     id: number;
     poster_path: string;
